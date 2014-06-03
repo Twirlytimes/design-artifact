e43e7387878577971cd412425c382fbbd1339c11
OpenShift guest <future.sparkle@gmail.com>
2014-06-02 22:33:01 -0400

    "Initial commit"

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DesignArtifact::Application.config.secret_key_base = 'e9abe1ddb699981edf7830876cf4aff80e9194e38cc160034c95e04ef349bf045f79da01e9d41085894fbcafcb94d148b44aaa5f5647c0d0fa0f8ceeaa8d68ec'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DesignArtifact::Application.config.session_store :cookie_store, key: '_design-artifact_session'
